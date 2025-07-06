e2a5f6b2e3a851021ca38326b117eb3197987603
Bicen <zhubicen@gmail.com>
2025-07-06 12:17:45 +0800

    y=0 can't be shown

--- examples/plots/mainwindow.cpp
+++ examples/plots/mainwindow.cpp
@@ -126,19 +126,25 @@ void MainWindow::setupQuadraticDemo(QCustomPlot *customPlot)
   demoName = "Quadratic Demo";
   // generate some data:
   QVector<double> x(101), y(101); // initialize with entries 0..100
+  QVector<double> x1(101), y1(101); // initialize with entries 0..100
+
   for (int i=0; i<101; ++i)
   {
-    x[i] = i/50.0 - 1; // x goes from -1 to 1
-    y[i] = x[i]*x[i];  // let's plot a quadratic function
+    x[i] = i;
+    y[i] = 0;
+    x1[i] = i;
+    y1[i] = 1;
   }
   // create graph and assign data to it:
   customPlot->addGraph();
   customPlot->graph(0)->setData(x, y);
+  customPlot->addGraph();
+  customPlot->graph(1)->setData(x1, y1);
   // give the axes some labels:
   customPlot->xAxis->setLabel("x");
   customPlot->yAxis->setLabel("y");
   // set axes ranges, so we see all data:
-  customPlot->xAxis->setRange(-1, 1);
+  customPlot->xAxis->setRange(0, 1);
   customPlot->yAxis->setRange(0, 1);
 }
 
